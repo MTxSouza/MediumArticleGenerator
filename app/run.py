f09c1b5c1c2063982f319d2f10aa6ba50426793b
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2024-05-02 23:02:56 -0300

    create: Defining router for inference.

--- app/run.py
+++ app/run.py
@@ -9,9 +9,9 @@ abs_path = os.path.join(os.sep, *abs_path)
 sys.path.append(abs_path)
 
 from fastapi import FastAPI, status
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, StreamingResponse
 
-from app.config import params
+from app.config import model, params
 
 app = FastAPI()
 
@@ -19,9 +19,14 @@ app = FastAPI()
 @app.get(path="/", status_code=status.HTTP_308_PERMANENT_REDIRECT)
 async def model_page():
     """Redirects to the Medium article of model."""
-    return RedirectResponse(url="/", status_code=status.HTTP_308_PERMANENT_REDIRECT)
+    return RedirectResponse(url="/about", status_code=status.HTTP_308_PERMANENT_REDIRECT)
 
 @app.get(path="/about", status_code=status.HTTP_200_OK)
 async def model_details():
     """Returns all hyper-parameters of model."""
     return params
+
+@app.get(path="/generate", status_code=status.HTTP_102_PROCESSING)
+async def generate(text: str = "<|sos|>", max_tokens: int = 100):
+    """Run the model inference and return it's generated text."""
+    return StreamingResponse(content=model.generate(text=text, max_len=max_tokens))
