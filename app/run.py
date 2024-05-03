9574b96e4614490c5ec4502e822858a09baa1106
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2024-05-02 22:54:24 -0300

    update: Sending model parameters with the /about route.

--- app/run.py
+++ app/run.py
@@ -11,7 +11,7 @@ sys.path.append(abs_path)
 from fastapi import FastAPI, status
 from fastapi.responses import RedirectResponse
 
-from app.schemas import ModelDetails
+from app.config import params
 
 app = FastAPI()
 
@@ -24,10 +24,4 @@ async def model_page():
 @app.get(path="/about", status_code=status.HTTP_200_OK)
 async def model_details():
     """Returns all hyper-parameters of model."""
-    return ModelDetails(
-        num_layers=6,
-        embedding_dim=768,
-        head_dim=64,
-        feed_forward_dim=3072,
-        context_window=512
-    )
+    return params
