8236b7ea8df4774e66776836d170c24fa92e83b4
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2024-05-09 22:16:27 -0300

    update: Reorganizing way to store the dataset and padding each article to the same length.

--- dev/tokenize_dataset.py
+++ dev/tokenize_dataset.py
@@ -8,6 +8,7 @@ For more information use --help command.
 import argparse
 import csv
 
+import numpy as np
 import tqdm
 
 from dev.utils.data import remove_links, remove_non_utf_8
@@ -30,6 +31,7 @@ def main():
     args = _arguments()
 
     # Define empty variables to store tokens and metadata
+    unique_tokens = []
     tokens = []
     custom_vocab = {}
     vocab_mapper = {}
@@ -72,20 +74,26 @@ def main():
             article_tokens = TikTokenizer.encode(text=text)
 
             # Filter articles based on length
-            if len(article_tokens) <= args.drop_long:
-                tokens.extend(article_tokens)  # Add tokens to the main list
+            article_length = len(article_tokens)
+            if article_length <= args.drop_long:
+                tokens.append(article_tokens)  # Add tokens to the main list
+                unique_tokens.extend(article_tokens)  # Add tokens to the unique list
 
-                if len(article_tokens) > longest_article:
-                    longest_article = len(article_tokens)
-                elif len(article_tokens) < shortest_article:
-                    shortest_article = len(article_tokens)
+                if article_length > longest_article:
+                    longest_article = article_length
+                elif article_length < shortest_article:
+                    shortest_article = article_length
         print(f"Number of articles: {len(tokens)}")
         print(f"Longest article: {longest_article} tokens.")
         print(f"Shortest article: {shortest_article} tokens.")
 
     # Create vocabulary and tokenizer after processing the entire dataset
     print("Retrieving unique tokens...")
-    unique_tokens = sorted(list(set(tokens + TikTokenizer.encode(text=TikTokenizer.UNK))))
+    unique_tokens = sorted(list(set(
+        unique_tokens + \
+        TikTokenizer.encode(text=TikTokenizer.UNK) + \
+        TikTokenizer.encode(text=TikTokenizer.PAD)
+    )))
     for token in tqdm.tqdm(iterable=unique_tokens, desc="Creating vocabulary..."):
         str_token = TikTokenizer.decode(tokens=[token])
         idx = len(custom_vocab)
@@ -93,15 +101,23 @@ def main():
         vocab_mapper[token] = idx
 
     # Remap the tokens to the new vocabulary
-    print("Remapping the tokens...")
-    tokens = list(map(vocab_mapper.get, tokens))
+    remapped_tokens = []
+    for token_seq in tqdm.tqdm(iterable=tokens, desc="Remapping the tokens..."):
+        remapped_tokens.append(list(map(vocab_mapper.get, token_seq)))
+
+    # Padding the tokens to the same length
+    for token_seq in tqdm.tqdm(iterable=remapped_tokens, desc=f"Padding the tokens to {longest_article} tokens..."):
+        token_seq.extend([TikTokenizer.INDEX_PAD] * (longest_article - len(token_seq)))
+
+    # Cast the tokens to numpy array
+    numpy_tokens = np.asarray(a=remapped_tokens, dtype=np.int32)
 
     # Saving results
     print("Saving the results...")
     create_directory(directory="source")
     save_json_file(filepath="./source/vocab.json", data=custom_vocab)
     save_json_file(filepath="./source/mapper.json", data=vocab_mapper)
-    save_numpy_file(filepath="./source/tokens.npz", data=tokens)
+    save_numpy_file(filepath="./source/tokens.npz", data=numpy_tokens)
 
     print("Done!")
 
